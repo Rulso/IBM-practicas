2a1351bd811c540d043468ec71bd8d59cac05096
Rulso <angelraulmaso@gmail.com>
2021-10-15 17:56:04 -0500

    Add files via upload

---
+++ Ej2/src/main/java/com/example/ej2/Ej2Application.java
@@ -0,0 +1,21 @@
+package com.example.ej2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableFeignClients
+public class Ej2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ej2Application.class, args);
+	}
+	
+
+}
